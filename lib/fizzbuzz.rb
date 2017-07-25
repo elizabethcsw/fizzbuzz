3c12da7492f34d6046c3d220ca2ab461c4525191
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-25 19:03:34 +0100

    Initialised 2 instance variables (language and num) and 1 instance method (fizzbuzz)

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,24 @@
-class Fixnum
+class Fixnumm
+  attr_accessor :language, :num
+
+  def initialize
+    @language = "English"
+    @num = num
+  end
+
   #this is a private method
   #this is a method that does not take an argument(num)
-  def fizzbuzz
-    return "fizzbuzz" if self%15 == 0
-    return "fizz" if self%3 == 0
-    return "buzz" if self%5 == 0
-    return self.to_s
+  def fizzbuzz(num)
+    return "fizzbuzz" if num%15 == 0
+    return "fizz" if num%3 == 0
+    return "buzz" if num%5 == 0
+    return num.to_s
   end
 end
 
-
+fizzy= Fixnumm.new
+puts fizzy.language
+puts fizzy.fizzbuzz(5)
 
 #warning: constant ::Fixnum is deprecated
 
@@ -20,4 +29,15 @@ def fizzbuzz(num)
   return "buzz" if num%5==0
   return num.to_s
 end
+
+class Fixnumm
+def fizzbuzz
+  return "fizzbuzz" if self%15 == 0
+  return "fizz" if self%3 == 0
+  return "buzz" if self%5 == 0
+  return self.to_s
+end
+end
+
+
 =end
