8b73d055f558207b2022542948f2f3f9c7590cd5
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-25 11:59:26 +0100

    Added a class outside it to make the private method work

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,9 @@
-def fizzbuzz(num)
-  return "fizzbuzz" if num%15==0
-  return "fizz" if num%3==0
-  return "buzz" if num%5==0
-  return num.to_s
+class Fiznum
+  #this is a private method
+  def fizzbuzz(num)
+    return "fizzbuzz" if num%15==0
+    return "fizz" if num%3==0
+    return "buzz" if num%5==0
+    return num.to_s
+  end
 end
