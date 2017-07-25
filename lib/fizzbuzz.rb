c43102d6af611f7fae31dbde65fedee73339dc16
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-07-25 12:38:08 +0100

    Revised spec files to test the ability of calling the fizzbuzz method (.fizzbuzz) in the Fixnum class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,22 @@
-class Fiznum
+class Fixnum
   #this is a private method
-  def fizzbuzz(num)
-    return "fizzbuzz" if num%15==0
-    return "fizz" if num%3==0
-    return "buzz" if num%5==0
-    return num.to_s
+  def fizzbuzz
+    return "fizzbuzz" if self%15 == 0
+    return "fizz" if self%3 == 0
+    return "buzz" if self%5 == 0
+    return self.to_s
   end
 end
+
+
+
+#warning: constant ::Fixnum is deprecated
+
+=begin
+def fizzbuzz(num)
+  return "fizzbuzz" if num%15==0
+  return "fizz" if num%3==0
+  return "buzz" if num%5==0
+  return num.to_s
+end
+=end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,29 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when passed 9' do
+    expect(9.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "fizz" when passed 17' do
+    expect(17.fizzbuzz).to eq '17'
+  end
+end
+
+=begin
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
@@ -21,3 +44,4 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(17)).to eq '17'
   end
 end
+=end
